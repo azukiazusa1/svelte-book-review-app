69592ae6b508836117d2f26ce8eca178e056c95e
azukiazusa1 <cycleippei@gmail.com>
2021-02-06 16:06:19 +0900

    remove

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,12 @@
   import Header from './components/Header.svelte'
   import Router from 'svelte-spa-router'
   import { routes } from './router'
-
-	export let name: string;
 </script>
 
 <Tailwindcss />
 <Header />
 <main class="pt-16">
   <div class="container mx-auto px-6 my-4">
-    <Router {routes}>
+    <Router {routes} />
   </div>
 </main>
