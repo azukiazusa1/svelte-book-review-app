463c5fa9ecfe815f43025f577618808f44edbd0e
azukiazusa1 <cycleippei@gmail.com>
2021-02-06 23:46:10 +0900

    fetch books

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,13 +1,43 @@
 <script lang="ts">
   import SearchBar from '../components/SearchBar.svelte'
+  import type { BookItem, Result } from '../repositories/book';
+  import RepositoryFactory, { BOOK } from '../repositories/RepositoryFactory'
+  const BookRepository = RepositoryFactory[BOOK]
 
-  let q = 'JavaScript'
+  let q = ''
+  let empty = false
+  let books: BookItem[] = []
+  let promise: Promise<void>
+
+  const handleSubmit = () => {
+    if (!q.trim()) return
+    promise = getBooks()
+  }
+
+  const getBooks = async () => {
+    books = []
+    empty = false
+    const result = await BookRepository.get({ q })
+    empty = result.totalItems === 0
+    books = result.items
+  }
 </script>
 
-<form>
+<form on:submit|preventDefault={handleSubmit}>
   <SearchBar bind:value={q} />
 </form>
 
 <div class="text-center mt-4">
-  { q }
+  {#if empty}
+    <div>検索結果が見つかりませんでした。</div>
+  {:else}
+    {#each books as book (book.id)}
+      <div>{book.volumeInfo.title}</div>
+    {/each}
+  {/if}
+  {#await promise}
+    <div>loading...</div>
+    {:catch e}
+      {e.message}
+  {/await}
 </div>
