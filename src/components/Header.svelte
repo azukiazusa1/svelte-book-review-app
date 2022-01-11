92844259fedc0a2a92ab41e6f2499d39d3bb68ce
torish14 <49876561+torish14@users.noreply.github.com>
2022-01-12 07:40:45 +0900

    タイポを修正

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,4 @@
-<header class="top-0 lef-0 w-full z-40 bg-gray-900 shadow fixed border-b border-gray-200">
+<header class="top-0 left-0 w-full z-40 bg-gray-900 shadow fixed border-b border-gray-200">
   <div class="container mx-auto px-6 h-16 flex justify-between items-center">
     <span class="font-semibold text-xl text-white tracking-tight">
       ブックレビュー App
